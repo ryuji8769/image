7b5629c34c40803bdae5e8ad46b4bf2715efdbda
ruiji <abcdabcd0517@icloud.com>
2021-07-08 00:38:08 +0900

    画像表示途中

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,16 @@ class PostsController < ApplicationController
   
   def new
   end
-  
+ 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(content: params[:content,])
+
     @post.save
     redirect_to("/posts/index")
+  end
+
+  def post_params
+    params.require(:post).permit(
+           :content, images: [])
   end
 end
