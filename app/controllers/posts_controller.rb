e5fac1d922651223416437a6d06e003709ad4f60
ruiji <abcdabcd0517@icloud.com>
2021-07-07 18:29:44 +0900

    posts_controllerとview_index.html.erb設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def new
+  end
+  
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
   end
 end
