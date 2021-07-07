9f87456de04dbbe90b14d266435e10eb4fb00b84
ruiji <abcdabcd0517@icloud.com>
2021-07-08 02:34:57 +0900

    ログイン完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,40 @@
 class PostsController < ApplicationController
+  protect_from_forgery
   def index
     @posts = Post.all.order(created_at: :desc)
   end
   
   def new
+    @posts = Post.new
   end
- 
-  def create
-    @post = Post.new(content: params[:content,])
 
-    @post.save
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
     redirect_to("/posts/index")
+    else
+      render :new
+    end
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id]) 
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update params.require(:post).permit(:content, images: [])
+    redirect_to @post
+  end
+
+
+  private
 
   def post_params
-    params.require(:post).permit(
-           :content, images: [])
+    params.require(:post).permit(:name, :images, :price, :evaluation, :description, :category_id, ).merge(user_id: current_user.id)
   end
 end
